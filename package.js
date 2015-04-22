19fdb03df785ac92b81066ff4018b16fc5afbe09
David Burles <dburles@gmail.com>
2015-04-22 12:14:02 +1000

    package.js & version bump

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
   name: "tmeasday:publish-counts",
   summary: "Publish the count of a cursor, in real time",
-  version: "0.3.7",
+  version: "0.3.8",
   git: "https://github.com/percolatestudio/publish-counts.git"
 });
 
 Package.on_use(function (api, where) {
   api.versionsFrom("METEOR@0.9.2");
-  api.use('blaze', 'client', { weak:true });
-  api.use('templating', 'client', {weak:true});
+  api.use(['blaze', 'templating'], 'client', { weak:true });
   api.use('mongo', 'client');
   api.add_files('publish-counts.js');
   api.export('Counts');
