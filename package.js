a8047f346df6b16e60ce0fe70a8a25dcadd0b0af
Tom Coleman <tom@thesnail.org>
2015-05-29 12:38:25 +1000

    0.4.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "tmeasday:publish-counts",
   summary: "Publish the count of a cursor, in real time",
-  version: "0.3.9",
+  version: "0.4.0",
   git: "https://github.com/percolatestudio/publish-counts.git"
 });
 
