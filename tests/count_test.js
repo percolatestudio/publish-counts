cc123014e00bfd7286e24fb941b84eeb6173b5ea
Justin Charette <charetjc@gmail.com>
2015-06-19 18:51:16 -0400

    split tests into separate files
    - create test helper file for shared collections, functions, and mock
      objects.
    - add tests for:
      - new subscriptions without records
      - new subscriptions with records
      - record addition
      - record removal
      - record updates
    - use difference between before and after to test effects of addition,
      update, and removal.
    
    Signed-off-by: Justin Charette <charetjc@gmail.com>

---
+++ tests/count_test.js
@@ -0,0 +1,80 @@
+if (Meteor.isServer) {
+
+  Meteor.publish('count', function (testId) {
+    Counts.publish(this, 'posts' + testId, Posts.find({ testId: testId }));
+  });
+
+  Meteor.methods({
+    setup_count: function (testId) {
+      H.insert(testId, 0, { name: "i'm a test post" });
+      H.insert(testId, 1, { name: "i'm a test post" });
+      H.insert(testId, 2, { name: "i'm a test post" });
+    },
+    addDoc_count: function (testId) {
+      H.insert(testId, 3, { name: "i'm a test post" });
+    },
+    updateDoc_count: function (testId) {
+      H.update(testId, 0, { $set: { name: "i'm an edited post" } });
+    },
+    removeDoc_count: function (testId) {
+      H.remove(testId, 0);
+    },
+  });
+}
+
+if (Meteor.isClient) {
+  Tinytest.addAsync("count: upon subscribe with no records, returns zero", function (test, done) {
+    Meteor.subscribe('count', test.id, function () {
+      test.equal(H.getCount(test.id), 0);
+      done();
+    });
+  });
+
+  Tinytest.addAsync("count: upon subscribe with records, returns number of records", function (test, done) {
+    Meteor.call('setup_count', test.id, function () {
+      Meteor.subscribe('count', test.id, function () {
+        test.equal(H.getCount(test.id), 3);
+        done();
+      });
+    });
+  });
+
+  Tinytest.addAsync("count: after adding a doc, increments count", function (test, done) {
+    Meteor.call('setup_count', test.id, function () {
+      Meteor.subscribe('count', test.id, function () {
+        var before = H.getCount(test.id);
+        Meteor.call('addDoc_count', test.id, function () {
+          var delta = H.getCount(test.id) - before;
+          test.equal(delta, +1);
+          done();
+        });
+      });
+    });
+  });
+
+  Tinytest.addAsync("count: after updating a doc, count remains the same", function (test, done) {
+    Meteor.call('setup_count', test.id, function () {
+      Meteor.subscribe('count', test.id, function () {
+        var before = H.getCount(test.id);
+        Meteor.call('updateDoc_count', test.id, function () {
+          var delta = H.getCount(test.id) - before;
+          test.equal(delta, 0);
+          done();
+        });
+      });
+    });
+  });
+
+  Tinytest.addAsync("count: after removing a doc, decrements count", function (test, done) {
+    Meteor.call('setup_count', test.id, function () {
+      Meteor.subscribe('count', test.id, function () {
+        var before = H.getCount(test.id);
+        Meteor.call('removeDoc_count', test.id, function () {
+          var delta = H.getCount(test.id) - before;
+          test.equal(delta, -1);
+          done();
+        });
+      });
+    });
+  });
+}
---
+++ tests/no_ready_test.js
@@ -0,0 +1,11 @@
+if (Meteor.isServer) {
+  Tinytest.add("noReady: when option is true, stops Counts.publish() from calling ready()", function(test) {
+    var pub = new H.PubMock();
+    Counts.publish(pub, 'posts' + test.id, Posts.find({ testId: test.id }));
+    test.isTrue(pub._ready);
+
+    pub = new H.PubMock();
+    Counts.publish(pub, 'posts' + test.id, Posts.find({ testId: test.id }), {noReady: true});
+    test.isFalse(pub._ready);
+  });
+}
