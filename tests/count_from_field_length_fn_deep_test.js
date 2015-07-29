3d84c8071f246e19a7fb2682b57139a2189f92e4
Justin Charette <charetjc@gmail.com>
2015-07-29 15:55:02 -0400

    fix: test deep properties in countFromFieldLength fn deep test
    Signed-off-by: Justin Charette <charetjc@gmail.com>

--- tests/count_from_field_length_fn_deep_test.js
+++ tests/count_from_field_length_fn_deep_test.js
@@ -1,19 +1,20 @@
 if (Meteor.isServer) {
   Meteor.publish('count_from_field_length_fn_deep', function (testId) {
-    Counts.publish(this, 'posts' + testId, Posts.find({testId: testId}), {countFromFieldLength: 'array'});
+    Counts.publish(this, 'posts' + testId, Posts.find({testId: testId}),
+        {countFromFieldLength: function (doc) { return doc.a.b; }});
   });
 
   Meteor.methods({
     setup_deep_countFromFieldLength_fn: function (testId) {
-      H.insert(testId, 0, {array: [1, 2, 3]});
-      H.insert(testId, 1, {array: [1, 2, 3, 4]});
+      H.insert(testId, 0, {a: {b: [1, 2, 3]}});
+      H.insert(testId, 1, {a: {b: [1, 2, 3, 4]}});
     },
     addDoc_deep_countFromFieldLength_fn: function (testId) {
-      H.insert(testId, 2, {array: [1, 2]});
+      H.insert(testId, 2, {a: {b: [1, 2]}});
     },
     updateDoc_deep_countFromFieldLength_fn: function (testId) {
       // add 2 elements to array of doc 0
-      H.update(testId, 0, {$set: {array: [1, 2, 3, 4, 5]}});
+      H.update(testId, 0, {$set: {'a.b': [1, 2, 3, 4, 5]}});
     },
     removeDoc_deep_countFromFieldLength_fn: function (testId) {
       H.remove(testId, 0);
