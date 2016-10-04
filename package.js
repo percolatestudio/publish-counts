4b4cebb39e46b2914f05b2ba4be9bd31b6c90960
Justin Charette <charetjc@gmail.com>
2016-10-04 16:18:02 -0400

    add underscore dependency to package test section
    Signed-off-by: Justin Charette <charetjc@gmail.com>

--- package.js
+++ package.js
@@ -19,6 +19,7 @@ Package.on_use(function (api, where) {
 Package.on_test(function (api) {
   api.use([
     'tmeasday:publish-counts',
+    'underscore',
     'tinytest',
     'mongo',
     'facts']);
